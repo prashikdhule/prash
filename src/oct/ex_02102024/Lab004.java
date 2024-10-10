30acc667bdfba263e86ac752d9f7f604d2e3e7c3
prashik <prashikdhule3@gmail.com>
2024-10-10 18:48:25 +0530

    today 10 oct

--- src/oct/ex_02102024/Lab004.java
+++ src/oct/ex_02102024/Lab004.java
@@ -5,8 +5,11 @@ import com.sun.tools.javac.Main;
 public class Lab004 {
 
     public static void main(String[] args) {
-        double age = 6.5 ;
+       // double age = 6.5 ;
         // int age = 75 ;
-        System.out.println(age);
+      //  long age = 93870337063l;
+       // boolean prashik = true;
+        //char grade = 'a';
+       //System.out.println(grade);
     }
 }
