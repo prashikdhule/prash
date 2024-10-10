00c6f9d6e8d7172797d9eb9a17519d401f1c1477
prashik <prashikdhule3@gmail.com>
2024-10-10 16:20:26 +0530

    prash

---
+++ src/oct/ex_02102024/Lab004.java
@@ -0,0 +1,12 @@
+package oct.ex_02102024;
+
+import com.sun.tools.javac.Main;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        double age = 6.5 ;
+        // int age = 75 ;
+        System.out.println(age);
+    }
+}
